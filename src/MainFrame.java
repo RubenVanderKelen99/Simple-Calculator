c2b325b492fbc0ce7e32c0b3ace44e8a498396c4
Ruben_Van_der_Kelen <ruben.vanderkelen@student.odisee.be>
2022-10-25 23:05:16 +0200

    Setup

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public void init() {
+        setTitle("test");
+        setSize(600, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);                
+    }    
+}
